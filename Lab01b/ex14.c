dd8530d141cff1db8f93b15e96e188f7252bb8d6
nicolasmelnik <nmelnik58@gmail.com>
2023-08-25 19:17:00 +0000

    Exercício 14.

---
+++ Lab01b/ex14.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+void insereCaracter(char str[100], int posicao, char c) {
+    if (posicao < 0 || posicao > strlen(str)) {
+        printf("Posição inválida.\n");
+        return;
+    }
+    for (int i = strlen(str); i >= posicao; i--) {
+        str[i + 1] = str[i];
+    }
+    str[posicao] = c; 
+}
+int main() {
+    char str[100] = "GOT"; 
+    int posicao = 2;
+    char caractere = 'A';
+    
+    printf("String antes da inserção: %s.\n", str);
+    insereCaracter(str, posicao, caractere);
+    printf("String após a inserção do caractere '%c' na posição %d: %s.\n", caractere, posicao, str);
+}
