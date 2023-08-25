29ff457da09fae9ced9327058229542b75985181
nicolasmelnik <nmelnik58@gmail.com>
2023-08-25 19:12:58 +0000

    Exercício 12.

---
+++ Lab01b/ex12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int encontrarPosicao(char c, char *string) {
+    int posicao = -1;
+
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] == c) {
+            posicao = i + 1;
+            break;
+        }
+    }
+    return posicao;
+}
+int main() {
+    int posicao = encontrarPosicao('a', "tomate");
+    if (posicao != -1) {
+        printf("O caractere 'a' é a %d° lestra (Posição %d da string) da palavra 'tomate'.\n", posicao, posicao - 1);
+    } else {
+        printf("O caractere não foi encontrado na string.\n");
+    }
+}
