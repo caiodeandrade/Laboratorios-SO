90d8a666bcb9122e8fc3ae6cc3d159a983e84be6
DiegoAluizio <diegoaluizio2004@gmail.com>
2023-08-28 19:24:28 +0000

    exercicio 4

---
+++ Lab01b/ex04.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char nome1[7] = "Ziraldo";
+    char nome2[3] = "Ana";
+
+    printf("Primeiro nome inserido: Ziraldo\n");
+
+    printf("Segundo nome inserido: Ana\n");
+
+    int comparison = strcmp(nome1, nome2);
+
+    if (comparison <= 0){
+        printf("Nomes em ordem alfabética: %s, %s\n", nome1, nome2);
+    }
+    else{
+        printf("Nomes em ordem alfabética: %s, %s\n", nome2, nome1);
+    }
+    return 0;
+}
