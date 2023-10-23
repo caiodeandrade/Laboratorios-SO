ea3f068fdb649c9df4a5b8ca2ae3f156473ab46a
nicolasmelnik <nmelnik58@gmail.com>
2023-10-23 19:52:20 +0000

    Multiplicação de Matrizes com Pthreads.

---
+++ Lab03a-Pthreads-Parte-I/multiplicacaoPthreads.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define N 10  // Tamanho das matrizes
+
+int A[N][N];
+int B[N][N];
+int C[N][N];
+
+void *multiply(void *arg) {
+    int *args = (int *)arg;
+    int row = args[0];
+    int col = args[1];
+
+    C[row][col] = 0;
+    for (int k = 0; k < N; k++) {
+        C[row][col] += A[row][k] * B[k][col];
+    }
+    pthread_exit(NULL);
+}
+
+void imprimirMatriz(int M[][N]) {
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N; j++) {
+			printf("%d ", M[i][j]);
+		}
+
+		printf("\n");
+	}
+}
+
+int main() {
+    int valor = 1;
+    
+    // Inicialize as matrizes A e B com valores fictícios
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = valor;
+            B[i][j] = valor;
+            valor++;
+        }
+    }
+    pthread_t threads[N][N];
+
+    // Crie as threads para calcular cada elemento de C
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            int *args = (int *)malloc(2 * sizeof(int));
+            args[0] = i;
+            args[1] = j;
+            pthread_create(&threads[i][j], NULL, multiply, args);
+        }
+    }
+    // Espere as threads terminarem
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            pthread_join(threads[i][j], NULL);
+        }
+    }
+    
+    printf("Matriz A:\n\n");
+    imprimirMatriz(A);
+    printf("\nMatriz B:\n\n");
+    imprimirMatriz(B);
+    printf("\nMatriz Resultante C (A x B):\n\n");
+    imprimirMatriz(C);
+
+    return 0;
+}
