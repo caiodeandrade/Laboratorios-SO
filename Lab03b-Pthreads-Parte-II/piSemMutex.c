127982b86fec5407f90b7dd35b54cf751074e810
nicolasmelnik <nmelnik58@gmail.com>
2023-10-27 20:45:06 +0000

    Estimando o valor de pi sem utilizar Mutex.

---
+++ Lab03b-Pthreads-Parte-II/piSemMutex.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define interacoes 1000000 
+
+int thread_count;
+long long n = interacoes; 
+double sum = 0.0;
+
+void* Thread_sum(void* rank) {
+    long my_rank = (long) rank;
+    double factor;
+    long long i;
+    long long my_n = n / thread_count;
+    long long my_first_i = my_n * my_rank;
+    long long my_last_i = my_first_i + my_n;
+
+    if (my_first_i % 2 == 0)
+        factor = 1.0;
+    else
+        factor = -1.0;
+
+    for (i = my_first_i; i < my_last_i; i++, factor = -factor) {
+        sum += factor / (2.0 * i + 1);
+    }
+
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Use: %s <number_of_threads>\n", argv[0]);
+        return 1;
+    }
+
+    thread_count = atoi(argv[1]);
+
+    pthread_t* thread_handles;
+    thread_handles = (pthread_t*)malloc(thread_count * sizeof(pthread_t));
+
+    for (long thread = 0; thread < thread_count; thread++) {
+        pthread_create(&thread_handles[thread], NULL, Thread_sum, (void*)thread);
+    }
+
+    for (long thread = 0; thread < thread_count; thread++) {
+        pthread_join(thread_handles[thread], NULL);
+    }
+
+    sum *= 4.0;
+    
+    printf("Número de threads utilizadas: %d", thread_count);
+    printf("\nValor estimado de pi: %.20lf\n\n", sum);
+
+    free(thread_handles);
+    return 0;
+}
